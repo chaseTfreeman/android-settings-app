8cc25fedce516a6b33e19d1f92e9c26f4bc03837
Milagros Rodriguez <sharmynrodriguez@gmail.com>
2020-10-25 21:18:12 -0500

    fix: check metadata id list

--- src/modules/userSyncTest/prepareMetadataSizeRequest.js
+++ src/modules/userSyncTest/prepareMetadataSizeRequest.js
@@ -1,3 +1,37 @@
+const getListElement = (orgUnit, elementType) => {
+    let idList = [],
+        valuesList = []
+
+    if (orgUnit[elementType].length > 0) {
+        orgUnit[elementType].forEach(elementOU => {
+            if (idList.length > 0) {
+                idList = idList.filter(element => element !== elementOU.id)
+                idList.push(elementOU.id)
+                valuesList = valuesList.filter(
+                    element => element.id !== elementOU.id
+                )
+                valuesList.push(elementOU)
+            } else {
+                idList.push(elementOU.id)
+                valuesList.push(elementOU)
+            }
+        })
+    }
+
+    return { idList, valuesList }
+}
+
+const getIdElementList = (idList, elementType) => {
+    if (idList.length > 0) {
+        idList = idList.filter(element => element !== elementType.id)
+        idList.push(elementType.id)
+    } else {
+        idList.push(elementType.id)
+    }
+
+    return idList
+}
+
 export const prepareMetadataRequest = orgUnitCollection => {
     let dataSetIdList = [],
         programsIdList = [],
@@ -13,68 +47,32 @@ export const prepareMetadataRequest = orgUnitCollection => {
     const indicatorValuesList = []
 
     orgUnitCollection.forEach(orgUnit => {
-        if (orgUnit.programs.length > 0) {
-            orgUnit.programs.forEach(programOU => {
-                if (programsIdList.length >= 1) {
-                    programsIdList = programsIdList.filter(
-                        program => program !== programOU.id
-                    )
-                    programsIdList.push(programOU.id)
-                    programsValuesList = programsValuesList.filter(
-                        program => program.id !== programOU.id
-                    )
-                    programsValuesList.push(programOU)
-                } else {
-                    programsIdList.push(programOU.id)
-                    programsValuesList.push(programOU)
-                }
-            })
-        }
+        const programResult = getListElement(orgUnit, 'programs')
+        const datasetResult = getListElement(orgUnit, 'dataSets')
 
-        if (orgUnit.dataSets.length > 0) {
-            orgUnit.dataSets.forEach(datasetOU => {
-                if (dataSetIdList.length >= 1) {
-                    dataSetIdList = dataSetIdList.filter(
-                        dataset => dataset !== datasetOU.id
-                    )
-                    dataSetIdList.push(datasetOU.id)
-                    dataSetValuesList = dataSetValuesList.filter(
-                        dataset => dataset.id !== datasetOU.id
-                    )
-                    dataSetValuesList.push(datasetOU)
-                } else {
-                    dataSetIdList.push(datasetOU.id)
-                    dataSetValuesList.push(datasetOU)
-                }
-            })
-        }
+        programsValuesList = programResult.valuesList
+        programsIdList = programResult.idList
+        dataSetValuesList = datasetResult.valuesList
+        dataSetIdList = datasetResult.idList
     })
 
     if (dataSetValuesList.length > 0) {
         dataSetValuesList.forEach(value => {
             const dataSetElement = value.dataSetElements
-            if (
-                //dataSetElement !== undefined &&
-                dataSetElement.length > 0
-            ) {
+            if (dataSetElement.length > 0) {
                 dataSetElement.forEach(value => {
                     if (Object.keys(value.dataElement).length !== 0) {
-                        if (dataElementListId.length >= 1) {
-                            dataElementListId = dataElementListId.filter(
-                                dataElement =>
-                                    dataElement !== value.dataElement.id
-                            )
-                            dataElementListId.push(value.dataElement.id)
-                        } else {
-                            dataElementListId.push(value.dataElement.id)
-                        }
+                        dataElementListId = getIdElementList(
+                            dataElementListId,
+                            value.dataElement
+                        )
                     }
                 })
             }
             const indicators = value.indicators
             if (indicators.length > 0) {
                 value.indicators.forEach(indicatorOU => {
-                    if (indicatorListId.length >= 1) {
+                    if (indicatorListId.length > 0) {
                         indicatorListId = indicatorListId.filter(
                             indicator => indicator !== indicatorOU.id
                         )
@@ -88,40 +86,27 @@ export const prepareMetadataRequest = orgUnitCollection => {
             }
             const categoryCombos = value.categoryCombo
             if (categoryCombos !== undefined) {
-                if (categoryComboListId.length >= 1) {
-                    categoryComboListId = categoryComboListId.filter(
-                        categoryCombo => categoryCombo !== categoryCombos.id
-                    )
-                    categoryComboListId.push(categoryCombos.id)
-                } else {
-                    categoryComboListId.push(categoryCombos.id)
-                }
+                categoryComboListId = getIdElementList(
+                    categoryComboListId,
+                    categoryCombos
+                )
 
                 if (value.categoryCombo.categories.length > 0) {
                     value.categoryCombo.categories.forEach(categoryValue => {
-                        if (categoryListId.length >= 1) {
-                            categoryListId = categoryListId.filter(
-                                category => category !== categoryValue.id
-                            )
-                            categoryListId.push(categoryValue.id)
-                        } else {
-                            categoryListId.push(categoryValue.id)
-                        }
+                        categoryListId = getIdElementList(
+                            categoryListId,
+                            categoryValue
+                        )
                     })
                 }
 
                 if (indicatorValuesList.length > 0) {
                     indicatorValuesList.forEach(value => {
-                        if (Object.keys(value.indicatorType).length !== 0) {
-                            if (indicatorTypeListId.length >= 1) {
-                                indicatorTypeListId = indicatorTypeListId.filter(
-                                    indicatorT =>
-                                        indicatorT !== value.indicatorType.id
-                                )
-                                indicatorTypeListId.push(value.indicatorType.id)
-                            } else {
-                                indicatorTypeListId.push(value.indicatorType.id)
-                            }
+                        if (Object.keys(value.indicatorType).length > 0) {
+                            indicatorTypeListId = getIdElementList(
+                                indicatorListId,
+                                value.indicatorType
+                            )
                         }
                     })
                 }
@@ -133,39 +118,20 @@ export const prepareMetadataRequest = orgUnitCollection => {
         programsValuesList.forEach(program => {
             const trackedEntityT = program.trackedEntityType
             if (trackedEntityT !== undefined) {
-                if (trackedEntityTypeListId.length >= 1) {
-                    trackedEntityTypeListId = trackedEntityTypeListId.filter(
-                        trackedEntity => trackedEntity !== trackedEntityT.id
-                    )
-                    trackedEntityTypeListId.push(trackedEntityT.id)
-                } else {
-                    trackedEntityTypeListId.push(trackedEntityT.id)
-                }
+                trackedEntityTypeListId = getIdElementList(
+                    trackedEntityTypeListId,
+                    trackedEntityT
+                )
             }
 
             const programTEA = program.programTrackedEntityAttributes
-            if (
-                //programTEA !== undefined &&
-                programTEA.length > 0
-            ) {
+            if (programTEA.length > 0) {
                 programTEA.forEach(value => {
-                    if (
-                        Object.keys(value.trackedEntityAttribute).length !== 0
-                    ) {
-                        if (optionSetListId.length >= 1) {
-                            optionSetListId = optionSetListId.filter(
-                                optionSet =>
-                                    optionSet !==
-                                    value.trackedEntityAttribute.optionSet.id
-                            )
-                            optionSetListId.push(
-                                value.trackedEntityAttribute.optionSet.id
-                            )
-                        } else {
-                            optionSetListId.push(
-                                value.trackedEntityAttribute.optionSet.id
-                            )
-                        }
+                    if (Object.keys(value.trackedEntityAttribute).length > 0) {
+                        optionSetListId = getIdElementList(
+                            optionSetListId,
+                            value.trackedEntityAttribute.optionSet
+                        )
                     }
                 })
             }
@@ -175,7 +141,6 @@ export const prepareMetadataRequest = orgUnitCollection => {
     return {
         datasetIdList: dataSetIdList,
         programIdList: programsIdList,
-
         trackedEntityTypeIdList: trackedEntityTypeListId,
         optionSetIdList: optionSetListId,
         dataElementIdList: dataElementListId,
