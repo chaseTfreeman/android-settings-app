bf5fe78dcc7e06f48b47c539946f024c94f0eeb3
Milagros <sharmynrodriguez@gmail.com>
2021-09-20 19:07:25 -0400

    feat: [program visualization] clean visualization validation

--- src/components/visualizationProgram/VisualizationSearch/helper.js
+++ src/components/visualizationProgram/VisualizationSearch/helper.js
@@ -1,3 +1,4 @@
+import mapValues from 'lodash/mapValues'
 import {
     LAST_12_MONTHS,
     LAST_12_WEEKS,
@@ -31,214 +32,50 @@ const COLUMN = 'COLUMN'
 const PIE = 'PIE'
 
 export const validateAndroidVisualization = visualizations => {
-    //console.log({ visualizations })
-    //return visualizations.map( visualization => checkVisualizationType(visualization))
     return visualizations.map(visualization =>
         checkVisualizationType(visualization)
     )
 }
 
 const checkVisualizationType = visualization => {
-    switch (visualization.type) {
-        case PIVOT_TABLE:
-            visualization.valid = true
-            //console.log('visualization', {visualization})
-            return true
-        case LINE:
-            visualization.valid = true
-            return true
-        case COLUMN:
-            visualization.valid = true
-            return true
-        case PIE:
-            visualization.valid = true
-            return true
-        case SINGLE_VALUE:
-            visualization.valid = true
-            return true
-        default:
-            visualization.valid = false
-            return true
+    if (isValidVisualizationType(visualization.type)) {
+        mapValues(visualization.relativePeriods, (period, i) => {
+            isValidPeriod(i) && period
+                ? (visualization.valid = true)
+                : (visualization.valid = false)
+        })
+    } else {
+        visualization.valid = false
     }
 }
 
-const isPivotValid = visualization => {
-    /*if (visualization.relativePeriods) {
+const relativePeriodsList = [
+    TODAY,
+    YESTERDAY,
+    LAST_3_DAYS,
+    LAST_7_DAYS,
+    LAST_14_DAYS,
+    LAST_30_DAYS,
+    THIS_WEEK,
+    LAST_WEEK,
+    LAST_4_WEEKS,
+    LAST_12_WEEKS,
+    THIS_MONTH,
+    LAST_MONTH,
+    LAST_3_MONTHS,
+    LAST_6_MONTHS,
+    LAST_12_MONTHS,
+    MONTHS_THIS_YEAR,
+    THIS_QUARTER,
+    LAST_QUARTER,
+    LAST_4_QUARTERS,
+    QUARTERS_THIS_YEAR,
+    THIS_YEAR,
+    LAST_YEAR,
+    LAST_5_YEAR,
+]
 
-    }*/
-    //visualization.relativePeriods[]
-}
+const isValidVisualizationType = visualizationType =>
+    [PIVOT_TABLE, LINE, COLUMN, PIE, SINGLE_VALUE].includes(visualizationType)
 
-const visualizationTypeMap = {
-    [PIVOT_TABLE]: {
-        relativePeriods: [
-            TODAY,
-            YESTERDAY,
-            LAST_3_DAYS,
-            LAST_7_DAYS,
-            LAST_14_DAYS,
-            LAST_30_DAYS,
-            THIS_WEEK,
-            LAST_WEEK,
-            LAST_4_WEEKS,
-            LAST_12_WEEKS,
-            THIS_MONTH,
-            LAST_MONTH,
-            LAST_3_MONTHS,
-            LAST_6_MONTHS,
-            LAST_12_MONTHS,
-            MONTHS_THIS_YEAR,
-            THIS_QUARTER,
-            LAST_QUARTER,
-            LAST_4_QUARTERS,
-            QUARTERS_THIS_YEAR,
-            THIS_YEAR,
-            LAST_YEAR,
-            LAST_5_YEAR,
-        ],
-        headers: 2,
-        columns: 2,
-    },
-    [LINE]: {
-        relativePeriods: [
-            TODAY,
-            YESTERDAY,
-            LAST_3_DAYS,
-            LAST_7_DAYS,
-            LAST_14_DAYS,
-            LAST_30_DAYS,
-            THIS_WEEK,
-            LAST_WEEK,
-            LAST_4_WEEKS,
-            LAST_12_WEEKS,
-            THIS_MONTH,
-            LAST_MONTH,
-            LAST_3_MONTHS,
-            LAST_6_MONTHS,
-            LAST_12_MONTHS,
-            MONTHS_THIS_YEAR,
-            THIS_QUARTER,
-            LAST_QUARTER,
-            LAST_4_QUARTERS,
-            QUARTERS_THIS_YEAR,
-            THIS_YEAR,
-            LAST_YEAR,
-            LAST_5_YEAR,
-        ],
-    },
-    [COLUMN]: {
-        relativePeriods: [
-            TODAY,
-            YESTERDAY,
-            LAST_3_DAYS,
-            LAST_7_DAYS,
-            LAST_14_DAYS,
-            LAST_30_DAYS,
-            THIS_WEEK,
-            LAST_WEEK,
-            LAST_4_WEEKS,
-            LAST_12_WEEKS,
-            THIS_MONTH,
-            LAST_MONTH,
-            LAST_3_MONTHS,
-            LAST_6_MONTHS,
-            LAST_12_MONTHS,
-            MONTHS_THIS_YEAR,
-            THIS_QUARTER,
-            LAST_QUARTER,
-            LAST_4_QUARTERS,
-            QUARTERS_THIS_YEAR,
-            THIS_YEAR,
-            LAST_YEAR,
-            LAST_5_YEAR,
-        ],
-    },
-    [PIE]: {
-        relativePeriods: [
-            TODAY,
-            YESTERDAY,
-            LAST_3_DAYS,
-            LAST_7_DAYS,
-            LAST_14_DAYS,
-            LAST_30_DAYS,
-            THIS_WEEK,
-            LAST_WEEK,
-            LAST_4_WEEKS,
-            LAST_12_WEEKS,
-            THIS_MONTH,
-            LAST_MONTH,
-            LAST_3_MONTHS,
-            LAST_6_MONTHS,
-            LAST_12_MONTHS,
-            MONTHS_THIS_YEAR,
-            THIS_QUARTER,
-            LAST_QUARTER,
-            LAST_4_QUARTERS,
-            QUARTERS_THIS_YEAR,
-            THIS_YEAR,
-            LAST_YEAR,
-            LAST_5_YEAR,
-        ],
-    },
-    [SINGLE_VALUE]: {
-        relativePeriods: [
-            TODAY,
-            YESTERDAY,
-            LAST_3_DAYS,
-            LAST_7_DAYS,
-            LAST_14_DAYS,
-            LAST_30_DAYS,
-            THIS_WEEK,
-            LAST_WEEK,
-            LAST_4_WEEKS,
-            LAST_12_WEEKS,
-            THIS_MONTH,
-            LAST_MONTH,
-            LAST_3_MONTHS,
-            LAST_6_MONTHS,
-            LAST_12_MONTHS,
-            MONTHS_THIS_YEAR,
-            THIS_QUARTER,
-            LAST_QUARTER,
-            LAST_4_QUARTERS,
-            QUARTERS_THIS_YEAR,
-            THIS_YEAR,
-            LAST_YEAR,
-            LAST_5_YEAR,
-        ],
-    },
-}
-
-/**
- * A valid array it is consider if every value is valid.
- *
- * A valid value it is consider if:
- * value == true
- * length > 0
- * */
-
-/*export const validateObjectByProperty = (propertyArray, object) =>
-    propertyArray.every(property => isValidValue(object[property]) === true)*/
-
-const isVisualizationType = item =>
-    !!visualizationTypeMap[item.type].isVisualizationType
-
-/*export const hasMapView = itemType =>
-    visualizationTypeMap[itemType].domainType === DOMAIN_TYPE_AGGREGATE*/
-/*export const isTrackerDomainType = itemType =>
-    visualizationTypeMap[itemType].domainType === DOMAIN_TYPE_TRACKER*/
-const getDefaultItemCount = itemType =>
-    visualizationTypeMap[itemType].defaultItemCount || 5
-//export const getAppName = itemType => visualizationTypeMap[itemType].appName || ''
-
-//create validation using some()
-
-/*
-* const ages = [3, 10, 18, 20];
-
-ages.some(checkAdult)   // Returns true
-
-function checkAdult(age) {
-  return age >= 18;
-}
-* */
+const isValidPeriod = period => relativePeriodsList.includes(period)
